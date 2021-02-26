2bc52ba7ae312f312137dac7b9adebfa260f17ce
Nick Mayhall <nmayhall@vt.edu>
2021-02-26 13:15:46 -0500

    up1

--- src/Indexing.jl
+++ src/Indexing.jl
@@ -2,9 +2,9 @@ using LinearAlgebra
 using StaticArrays
 
 
-#Base.convert(::Type{Tuple{T,T}}, in::Tuple{T2,T2}) where {T,T2} = ( convert(T, in[1]), convert(T, in[2]) )
+Base.convert(::Type{Tuple{T,T}}, in::Tuple{T2,T2}) where {T,T2} = ( convert(T, in[1]), convert(T, in[2]) )
 
-#Base.convert(::Type{NTuple{N, Tuple{T,T}}}, in::Vector{Tuple{T2,T2}}) where {T,N,T2} = ntuple(i -> convert(Tuple{T,T}, in[i]), length(in))
+Base.convert(::Type{NTuple{N, Tuple{T,T}}}, in::Vector{Tuple{T2,T2}}) where {T,N,T2} = ntuple(i -> convert(Tuple{T,T}, in[i]), length(in))
 #Base.convert(::Type{NTuple{N, Tuple{T,T}}}, in::Vector{Tuple{T,T}}) where {T,N}      = ntuple(i -> in[i], length(in))
 
 """
@@ -15,19 +15,19 @@ const TransferConfig{N}   = NTuple{N,Tuple{Int16,Int16}}
 const TuckerConfig{N}     = NTuple{N,UnitRange{UInt16}}
 const OperatorConfig{N,T} = Tuple{FockConfig{N}, FockConfig{N}, T, T}
 
-Base.isequal(x::FockConfig, y::FockConfig) = all([all(isequal.(x[i],y[i])) for i in 1:length(x.config)])
-Base.isequal(x::TransferConfig, y::TransferConfig) = all([all(isequal.(x[i],y[i])) for i in 1:length(x.config)])
-#Base.isequal(x::ClusterConfig, y::ClusterConfig) = all(isequal.(x.config, y.config))
-Base.isequal(x::OperatorConfig, y::OperatorConfig) = all(isequal.(x.config, y.config))
-Base.isequal(x::TuckerConfig, y::TuckerConfig) = all(isequal.(x.config, y.config))
-
-Base.:(==)(x::FockConfig, y::FockConfig) = all([all(x[i].==y[i]) for i in 1:length(x.config)])
-Base.:(==)(x::TransferConfig, y::TransferConfig) = all([all(x[i].==y[i]) for i in 1:length(x.config)])
-#Base.:(==)(x::ClusterConfig, y::ClusterConfig) = all(x.config .== y.config)
-Base.:(==)(x::TuckerConfig, y::TuckerConfig) = all(x.config .== y.config)
-
-Base.size(tc::TuckerConfig) = Tuple(length.(tc.config))
-Base.push!(tc::TuckerConfig, range) = push!(tc.config,range)
+#Base.isequal(x::FockConfig, y::FockConfig) = all([all(isequal.(x[i],y[i])) for i in 1:length(x.config)])
+#Base.isequal(x::TransferConfig, y::TransferConfig) = all([all(isequal.(x[i],y[i])) for i in 1:length(x.config)])
+##Base.isequal(x::ClusterConfig, y::ClusterConfig) = all(isequal.(x.config, y.config))
+#Base.isequal(x::OperatorConfig, y::OperatorConfig) = all(isequal.(x.config, y.config))
+#Base.isequal(x::TuckerConfig, y::TuckerConfig) = all(isequal.(x.config, y.config))
+#
+#Base.:(==)(x::FockConfig, y::FockConfig) = all([all(x[i].==y[i]) for i in 1:length(x.config)])
+#Base.:(==)(x::TransferConfig, y::TransferConfig) = all([all(x[i].==y[i]) for i in 1:length(x.config)])
+##Base.:(==)(x::ClusterConfig, y::ClusterConfig) = all(x.config .== y.config)
+#Base.:(==)(x::TuckerConfig, y::TuckerConfig) = all(x.config .== y.config)
+#
+#Base.size(tc::TuckerConfig) = Tuple(length.(tc.config))
+#Base.push!(tc::TuckerConfig, range) = push!(tc.config,range)
 
 
 function Base.:(+)(x::FockConfig{N}, y::TransferConfig{N}) where N
