b4d5881e7911a739706b13d021aa39918851354c
Nick Mayhall <nmayhall@vt.edu>
2021-03-11 11:25:40 -0500

    not sure why this is changing things

--- src/Indexing.jl
+++ src/Indexing.jl
@@ -7,10 +7,10 @@ abstract type SparseIndex end
 
 @inline Base.length(a::SparseIndex) = length(a.config)
 @inline Base.getindex(s::SparseIndex, i) = s.config[i]
-@inline Base.hash(a::SparseIndex) = a.hash
+#@inline Base.hash(a::SparseIndex) = a.hash
 @inline Base.isequal(x::SparseIndex, y::SparseIndex) = isequal(x.hash, y.hash) 
 Base.:(==)(x::SparseIndex, y::SparseIndex) = x.hash == y.hash 
-#@inline Base.hash(a::SparseIndex) = hash(a.config)
+@inline Base.hash(a::SparseIndex) = hash(a.config)
 #@inline Base.isequal(x::SparseIndex, y::SparseIndex) = isequal(x.config, y.config) 
 #Base.:(==)(x::SparseIndex, y::SparseIndex) = x.config == y.config 
 Base.iterate(conf::SparseIndex, state=1) = iterate(conf.config, state)
