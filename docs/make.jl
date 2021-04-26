c0a90b640de8844455d15cd2166691c3be6cd2b7
Nick Mayhall <nmayhall@vt.edu>
2021-04-26 16:44:02 -0400

    change

--- docs/make.jl
+++ docs/make.jl
@@ -56,7 +56,7 @@ examples = [
 makedocs(
     modules=[FermiCG],
     authors="Nick Mayhall <nmayhall@vt.edu> and contributors",
-    repo="https://github.com/nmayhall-vt/FermiCG/blob/{commit}{path}#L{line}",
+    repo="github.com/nmayhall-vt/FermiCG/blob/{commit}{path}#L{line}",
     sitename="FermiCG.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
