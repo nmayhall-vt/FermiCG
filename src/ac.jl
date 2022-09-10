5ed7d118462244c4997e7ecdf36c472250063f26
Nick Mayhall <nmayhall@vt.edu>
2022-09-10 11:09:54 -0400

    new approach seems to work well - need to double check

--- src/ac.jl
+++ src/ac.jl
@@ -3,6 +3,117 @@ using Polynomials
 
 
 """
+    compute_ac_fixed(v_cmf::BSTstate{T,N,R}, 
+                    cluster_ops::Vector{ClusterOps{T}}, 
+                    clustered_ham::ClusteredOperator{T,N}, 
+                    lambda_grid::Vector{T},
+                    thresh_var  = 1e-1,
+                    thresh_foi  = 1e-4,
+                    thresh_pt   = 1e-3;
+                    h0="Hcmf") where {N,T,R}
+
+Form a hamiltonian to diagonalize for a give point on an adiabatic connection path
+H(λ) = H0 + λ*(H - H0)
+
+H0 = Hcmf + <|H-Hcmf|>
+   = Hcmf + <V> 
+
+H(λ) = Hcmf + <V> + λ*(H - Hcmf - <V>)
+     = (1-λ)*Hcmf + λ*H + (1-λ)<V> 
+
+dE/dλ  (@λ') = <λ'|H-H0|λ'> 
+       = <λ'|H|λ'> - <λ'|Hcmf|λ'> - <V> 
+"""
+function compute_ac_fixed(v_cmf::BSTstate{T,N,R}, 
+                    cluster_ops::Vector{ClusterOps{T}}, 
+                    clustered_ham::ClusteredOperator{T,N}, 
+                    lambda_ref::T,
+                    lambda_grid::Vector{T};
+                    thresh_var  = 1e-2,
+                    thresh_foi  = 1e-6,
+                    thresh_pt   = 1e-5,
+                    h0="Hcmf") where {N,T,R}
+
+
+    H = deepcopy(clustered_ham) 
+    H0 = build_1B_operator(v_cmf.clusters, op_string = "Hcmf", T=T)
+    
+    E = compute_expectation_value(v_cmf, cluster_ops, H)[1]
+    E0 = compute_expectation_value(v_cmf, cluster_ops, H0)[1]
+   
+    Eshift = E-E0
+    
+    Eλ = Vector{Vector{T}}([]) 
+    dEλ = Vector{Vector{T}}([]) 
+    dims = Vector{Int}([]) 
+    times = Vector{T}([]) 
+
+    v = deepcopy(v_cmf)
+   
+
+    # compute BST space with reference lambda
+    λ = lambda_ref
+    @printf(" Reference λ = %12.8f\n", λ)
+    H0_curr = deepcopy(H0) 
+    H_curr = deepcopy(H) 
+
+    scale!(H_curr, λ)
+    scale!(H0_curr, 1-λ)
+
+    Hλ = H_curr + H0_curr
+
+    e_var, v = FermiCG.block_sparse_tucker(v, cluster_ops, Hλ,
+                                               max_iter    = 20,
+                                               max_iter_pt = 200, 
+                                               nbody       = 4,
+                                               H0          = "Hcmf",
+                                               thresh_var  = thresh_var,
+                                               thresh_foi  = thresh_foi,
+                                               thresh_pt   = thresh_pt,
+                                               ci_conv     = 1e-5,
+                                               do_pt       = true,
+                                               resolve_ss  = false, 
+                                               tol_tucker  = 1e-4)
+
+    for λ in lambda_grid
+        @printf(" λ = %12.8f\n", λ)
+        H0_curr = deepcopy(H0) 
+        H_curr = deepcopy(H) 
+        
+        scale!(H_curr, λ)
+        scale!(H0_curr, 1-λ)
+        
+        Hλ = H_curr + H0_curr
+            
+        time = @elapsed e_var, v = ci_solve(v, cluster_ops, Hλ, 
+                            conv_thresh = 1e-6,
+                            max_iter    = 100,
+                            max_ss_vecs = 12,
+                            nbody       = 4)
+     
+        push!(Eλ, e_var .+ (1-λ)*Eshift)
+       
+        E = compute_expectation_value(v, cluster_ops, H)
+        E0 = compute_expectation_value(v, cluster_ops, H0)
+        push!(dEλ, E .- E0 .- Eshift)
+        push!(dims, length(v))
+        push!(times, time)
+    end
+
+    Eλout = zeros(T,length(lambda_grid), R)
+    dEλout = zeros(T,length(lambda_grid), R)
+
+    for i in 1:length(lambda_grid)
+        for j in 1:R
+            Eλout[i,j] = Eλ[i][j]
+            dEλout[i,j] = dEλ[i][j]
+        end
+    end
+
+    return lambda_grid, Eλout, dEλout, dims, times
+end
+
+"""
     compute_ac(v_cmf::BSTstate{T,N,R}, 
                     cluster_ops::Vector{ClusterOps{T}}, 
                     clustered_ham::ClusteredOperator{T,N}, 
