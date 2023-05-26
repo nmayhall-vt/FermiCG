15852291315ac6f1c98d564598e59c6f0aaec726
Nick Mayhall <nmayhall@vt.edu>
2023-05-26 07:42:03 -0400

    removed nbody from the CI part of bst

--- src/bst.jl
+++ src/bst.jl
@@ -527,7 +527,6 @@ function block_sparse_tucker(input_vec::BSTstate{T,N,R}, cluster_ops, clustered_
             conv_thresh=ci_conv,
             max_iter=ci_max_iter,
             max_ss_vecs=ci_max_ss_vecs,
-            nbody=nbody,
             lindep_thresh=ci_lindep_thresh,
             solver=solver)
         push!(e_variational_list, e_var)
