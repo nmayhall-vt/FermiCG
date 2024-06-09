d5625a41112bd38f7a2a22a0c88bcfa98945946b
Arnab Bachhar <86238928+arnab82@users.noreply.github.com>
2024-06-09 17:18:11 -0400

    Update tpsci_pt2_energy.jl

--- src/tpsci_pt2_energy.jl
+++ src/tpsci_pt2_energy.jl
@@ -128,7 +128,11 @@ function compute_pt2_energy(ci_vector_in::TPSCIstate{T,N,R}, cluster_ops, cluste
     flush(stdout)
 
 
+    #tmp = Int(round(length(jobs_vec)/100))
     tmp = Int(round(length(jobs_vec)/100))
+    if tmp == 0
+        tmp += 1
+    end
     verbose < 1 || println("   |----------------------------------------------------------------------------------------------------|")
     verbose < 1 || println("   |0%                                                                                              100%|")
     verbose < 1 || print("   |")
